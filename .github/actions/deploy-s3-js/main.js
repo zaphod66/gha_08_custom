020bb53d348e94c9fc1dad0bdccefcd6ae29b643
Norbert Scheller <norbert@riskident.com>
2026-07-24 19:31:45 +0200

    adjusted main.js

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,6 +1,6 @@
-const core = require('@actions/core');
-const exec = require('@actions/exec');
-const github = require('@actions/github');
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import * as github from '@actions/github';
 
 function run() {
   core.notice('Deploying to AWS S3 using JS');
